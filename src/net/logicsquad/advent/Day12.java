b3a58a014c9353938e43eb0574e4edbc4de37243
Paul Hoadley <paulh@logicsquad.net>
2015-12-26 13:15:20 +1030

    Completes Day 12 part 1.

---
+++ src/net/logicsquad/advent/Day12.java
@@ -0,0 +1,40 @@
+package net.logicsquad.advent;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+public class Day12 {
+	private static final String INPUT_FILENAME = "etc/day12.input";
+
+	public static void main(String[] args) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(INPUT_FILENAME));
+		String string = new String(encoded, "UTF-8");
+		Gson gson = new Gson();
+		Object json = gson.fromJson(string, Object.class);
+		System.out.println("Day12.main: sum = " + sumOfNumbers(json));
+	}
+
+	private static int sumOfNumbers(Object node) {
+		int result = 0;
+		if (node instanceof List<?>) {
+			for (Object o : (List<?>) node) {
+				result += sumOfNumbers(o);
+			}
+		} else if (node instanceof Map<?, ?>) {
+			for (Object o : ((Map<?, ?>) node).keySet()) {
+				result += sumOfNumbers(((Map<?, ?>) node).get(o));
+			}
+		} else {
+			try {
+				result += Float.parseFloat(node.toString());
+			} catch (NumberFormatException e) {
+			}
+		}
+		return result;
+	}
+}
