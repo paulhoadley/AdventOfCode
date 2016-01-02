6f7ea9d30e9a207db2f23b40fb692279e85c0d11
Paul Hoadley <paulh@logicsquad.net>
2016-01-02 20:30:45 +1030

    Completes Day 17 part 2.

--- src/net/logicsquad/advent/Day17.java
+++ src/net/logicsquad/advent/Day17.java
@@ -5,7 +5,10 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import net.logicsquad.advent.utils.Combination;
 
@@ -15,12 +18,12 @@ public class Day17 {
 
 	public static void main(String[] args) throws IOException {
 		List<Integer> ints = new ArrayList<Integer>();
+		Map<Integer, Integer> leaderboard = new HashMap<Integer, Integer>();
 		List<String> lines = Files.readAllLines(Paths.get(INPUT_FILENAME),
 				StandardCharsets.UTF_8);
 		for (String line : lines) {
 			ints.add(Integer.parseInt(line));
 		}
-		int result = 0;
 		for (int i = 1; i <= ints.size(); i++) {
 			Combination<Integer> comb = new Combination<Integer>(ints, i);
 			for (List<Integer> candidate : comb) {
@@ -29,11 +32,17 @@ public class Day17 {
 					total += j;
 				}
 				if (total == TOTAL) {
-					result++;
+					Integer k = leaderboard.get(i);
+					if (k == null) {
+						k = Integer.valueOf(0);
+					}
+					leaderboard.put(i, ++k);
 				}
 			}
 		}
-		System.out.println("Day17.main: result = " + result);
+		List<Integer> leaders = new ArrayList<Integer>(leaderboard.keySet());
+		Collections.sort(leaders);
+		System.out.println("Day17.main: result = " + leaderboard.get(leaders.get(0)));
 		return;
 	}
 }
