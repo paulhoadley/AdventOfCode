2bb40488c0488fea80947f650fe0ab0b05075422
Paul Hoadley <paulh@logicsquad.net>
2016-01-11 20:18:32 +1030

    Light was only pretending to be a boolean.

--- src/net/logicsquad/advent/Day18.java
+++ src/net/logicsquad/advent/Day18.java
@@ -6,8 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-import net.logicsquad.advent.Day18.Light.State;
-
 public class Day18 {
 	private static final String INPUT_FILENAME = "etc/day18.input";
 
@@ -20,29 +18,17 @@ public class Day18 {
 		System.out.println("Day18.main: grid.lightsOn() = " + grid.lightsOn());
 	}
 
-	public static class Light {
-		public State state;
-
-		public enum State {
-			ON, OFF;
-		}
-
-		public Light(State state) {
-			this.state = state;
-		}
-	}
-
 	public static class Grid {
-		private Light[][] lights = new Light[100][100];
+		private boolean[][] lights = new boolean[100][100];
 
 		public Grid(List<String> input) {
 			for (int i = 0; i < 100; i++) {
 				for (int j = 0; j < 100; j++) {
 					char c = input.get(i).charAt(j);
 					if (c == '#') {
-						lights[i][j] = new Light(State.ON);
+						lights[i][j] = true;
 					} else {
-						lights[i][j] = new Light(State.OFF);
+						lights[i][j] = false;
 					}
 				}
 			}
@@ -50,49 +36,49 @@ public class Day18 {
 
 		private int neighboursAlight(int i, int j) {
 			int count = 0;
-			if (i - 1 > -1 && j - 1 > -1 && lights[i - 1][j - 1].state == State.ON) {
+			if (i - 1 > -1 && j - 1 > -1 && lights[i - 1][j - 1] == true) {
 				count++;
 			}
-			if (j - 1 > -1 && lights[i][j - 1].state == State.ON) {
+			if (j - 1 > -1 && lights[i][j - 1] == true) {
 				count++;
 			}
-			if (i + 1 < 100 && j - 1 > -1 && lights[i + 1][j - 1].state == State.ON) {
+			if (i + 1 < 100 && j - 1 > -1 && lights[i + 1][j - 1] == true) {
 				count++;
 			}
-			if (i - 1 > -1 && lights[i - 1][j].state == State.ON) {
+			if (i - 1 > -1 && lights[i - 1][j] == true) {
 				count++;
 			}
-			if (i + 1 < 100 && lights[i + 1][j].state == State.ON) {
+			if (i + 1 < 100 && lights[i + 1][j] == true) {
 				count++;
 			}
-			if (i - 1 > -1 && j + 1 < 100 && lights[i - 1][j + 1].state == State.ON) {
+			if (i - 1 > -1 && j + 1 < 100 && lights[i - 1][j + 1] == true) {
 				count++;
 			}
-			if (j + 1 < 100 && lights[i][j + 1].state == State.ON) {
+			if (j + 1 < 100 && lights[i][j + 1] == true) {
 				count++;
 			}
-			if (i + 1 < 100 && j + 1 < 100 && lights[i + 1][j + 1].state == State.ON) {
+			if (i + 1 < 100 && j + 1 < 100 && lights[i + 1][j + 1] == true) {
 				count++;
 			}
 			return count;
 		}
 
 		public void tick() {
-			Light[][] next = new Light[100][100];
+			boolean[][] next = new boolean[100][100];
 			for (int i = 0; i < 100; i++) {
 				for (int j = 0; j < 100; j++) {
 					int alight = neighboursAlight(i, j);
-					if (lights[i][j].state == State.ON) {
+					if (lights[i][j] == true) {
 						if (alight == 2 || alight == 3) {
-							next[i][j] = new Light(State.ON);
+							next[i][j] = true;
 						} else {
-							next[i][j] = new Light(State.OFF);
+							next[i][j] = false;
 						}
 					} else {
 						if (alight == 3) {
-							next[i][j] = new Light(State.ON);
+							next[i][j] = true;
 						} else {
-							next[i][j] = new Light(State.OFF);
+							next[i][j] = false;
 						}
 					}
 				}
@@ -105,7 +91,7 @@ public class Day18 {
 			int count = 0;
 			for (int i = 0; i < 100; i++) {
 				for (int j = 0; j < 100; j++) {
-					if (lights[i][j].state == State.ON) {
+					if (lights[i][j] == true) {
 						count++;
 					}
 				}
