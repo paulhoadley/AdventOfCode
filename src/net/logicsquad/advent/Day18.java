56d9f3165b0a8fbfd0ab6a64a9c329885ebcbca9
Paul Hoadley <paulh@logicsquad.net>
2016-01-11 20:38:56 +1030

    Phil Roberts version.

--- src/net/logicsquad/advent/Day18.java
+++ src/net/logicsquad/advent/Day18.java
@@ -7,65 +7,47 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Day18 {
-	private static final String INPUT_FILENAME = "etc/day18.input";
-
 	public static void main(String[] args) throws IOException {
-		List<String> lines = Files.readAllLines(Paths.get(INPUT_FILENAME), StandardCharsets.UTF_8);
-		Grid grid = new Grid(lines);
-		for (int i = 0; i < 100; i++) {
-			grid.tick();
-		}
-		System.out.println("Day18.main: grid.lightsOn() = " + grid.lightsOn());
+		G g = new G(Files.readAllLines(Paths.get("etc/day18.input"), StandardCharsets.UTF_8));
+		for (int i = 0; i < 100; i++) g.t();
+		System.out.println("Day18.main: grid.lightsOn() = " + g.o());
 	}
-
-	public static class Grid {
-		private boolean[][] lights = new boolean[100][100];
-
-		public Grid(List<String> input) {
-			for (int i = 0; i < 100; i++) {
-				for (int j = 0; j < 100; j++) {
-					char c = input.get(i).charAt(j);
-					if (c == '#') lights[i][j] = true;
-				}
-			}
+	public static class G {
+		private boolean[][] l = new boolean[100][100];
+		public G(List<String> p) {
+			for (int i = 0; i < 100; i++) for (int j = 0; j < 100; j++) if (p.get(i).charAt(j) == '#') l[i][j] = true;
 		}
-
-		private int neighboursAlight(int i, int j) {
+		private int n(int i, int j) {
 			int c = 0;
-			if (i - 1 > -1 && j - 1 > -1 && lights[i - 1][j - 1]) c++;
-			if (j - 1 > -1 && lights[i][j - 1]) c++;
-			if (i + 1 < 100 && j - 1 > -1 && lights[i + 1][j - 1]) c++;
-			if (i - 1 > -1 && lights[i - 1][j]) c++;
-			if (i + 1 < 100 && lights[i + 1][j]) c++;
-			if (i - 1 > -1 && j + 1 < 100 && lights[i - 1][j + 1]) c++;
-			if (j + 1 < 100 && lights[i][j + 1]) c++;
-			if (i + 1 < 100 && j + 1 < 100 && lights[i + 1][j + 1]) c++;
+			if (i - 1 > -1 && j - 1 > -1 && l[i - 1][j - 1]) c++;
+			if (j - 1 > -1 && l[i][j - 1]) c++;
+			if (i + 1 < 100 && j - 1 > -1 && l[i + 1][j - 1]) c++;
+			if (i - 1 > -1 && l[i - 1][j]) c++;
+			if (i + 1 < 100 && l[i + 1][j]) c++;
+			if (i - 1 > -1 && j + 1 < 100 && l[i - 1][j + 1]) c++;
+			if (j + 1 < 100 && l[i][j + 1]) c++;
+			if (i + 1 < 100 && j + 1 < 100 && l[i + 1][j + 1]) c++;
 			return c;
 		}
-
-		public void tick() {
-			boolean[][] next = new boolean[100][100];
-			for (int i = 0; i < 100; i++) {
+		public void t() {
+			boolean[][] x = new boolean[100][100];
+			for (int i = 0; i < 100; i++)
 				for (int j = 0; j < 100; j++) {
-					int alight = neighboursAlight(i, j);
-					if (lights[i][j]) {
-						if (alight == 2 || alight == 3) next[i][j] = true;
+					int a = n(i, j);
+					if (l[i][j]) {
+						if (a == 2 || a == 3)
+							x[i][j] = true;
 					} else {
-						if (alight == 3) next[i][j] = true;
+						if (a == 3)
+							x[i][j] = true;
 					}
 				}
-			}
-			lights = next;
+			l = x;
 			return;
 		}
-
-		public int lightsOn() {
+		public int o() {
 			int c = 0;
-			for (int i = 0; i < 100; i++) {
-				for (int j = 0; j < 100; j++) {
-					if (lights[i][j]) c++;
-				}
-			}
+			for (int i = 0; i < 100; i++) for (int j = 0; j < 100; j++) if (l[i][j]) c++;
 			return c;
 		}
 	}
