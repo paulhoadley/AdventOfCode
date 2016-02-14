d337c6c8bf4895df425acb1c5333bafa87f30ee2
Paul Hoadley <paulh@logicsquad.net>
2016-02-14 21:33:12 +1030

    Completes Day 20 part 2.

--- src/net/logicsquad/advent/Day20.java
+++ src/net/logicsquad/advent/Day20.java
@@ -2,17 +2,20 @@ package net.logicsquad.advent;
 
 public class Day20 {
 	private static final int MIN_SIZE = 34000000;
-	
+
 	public static void main(String[] args) {
 		int house = 1;
 		while (true) {
 			int total = 0;
 			for (int elf = 1; elf <= house; elf++) {
+				if (house > elf * 50) {
+					continue;
+				}
 				if (house % elf == 0) {
-					total += elf * 10;
+					total += elf * 11;
 				}
 			}
-			if (total > MIN_SIZE) {
+			if (total >= MIN_SIZE) {
 				break;
 			}
 			house++;
