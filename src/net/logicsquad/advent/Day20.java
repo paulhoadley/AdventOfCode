c9b0c0e0b026456fdea929a56aecf237d99af7d9
Paul Hoadley <paulh@logicsquad.net>
2016-02-15 19:49:02 +1030

    Adds optimisation from Phil Roberts.

--- src/net/logicsquad/advent/Day20.java
+++ src/net/logicsquad/advent/Day20.java
@@ -4,15 +4,13 @@ public class Day20 {
 	private static final int MIN_SIZE = 34000000;
 
 	public static void main(String[] args) {
+		long start = System.nanoTime();
 		int house = 1;
 		while (true) {
 			int total = 0;
-			for (int elf = 1; elf <= house; elf++) {
-				if (house > elf * 50) {
-					continue;
-				}
+			for (int elf = 1; elf < 51; elf++) {
 				if (house % elf == 0) {
-					total += elf * 11;
+					total += 11 * house / elf;
 				}
 			}
 			if (total >= MIN_SIZE) {
@@ -21,5 +19,8 @@ public class Day20 {
 			house++;
 		}
 		System.out.println("Day20.main: house = " + house);
+		long end = System.nanoTime();
+		float duration = (end - start) / 1000000;
+		System.out.println("Day20.main: duration = " + duration);
 	}
 }
