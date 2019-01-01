3cf0d14bfafd3221106ba7d64c5ab1cc4bd1c0bd
Paul Hoadley <paulh@logicsquad.net>
2019-01-01 14:55:17 +1030

    Modifies Day1 to solve part 2.

--- src/net/logicsquad/advent/y2018/Day1.java
+++ src/net/logicsquad/advent/y2018/Day1.java
@@ -3,12 +3,32 @@ package net.logicsquad.advent.y2018;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day1 {
 	private static final String INPUT_FILENAME = "etc/2018/day1.input";
 
 	public static void main(String args[]) throws IOException {
-		System.out.println("sum = " + Files.lines(Paths.get(INPUT_FILENAME)).mapToInt(Integer::valueOf).sum());
-		return;
+		try (Stream<String> values = Files.lines(Paths.get(INPUT_FILENAME))) {
+			List<Integer> ints = values.map(s -> Integer.valueOf(s)).collect(Collectors.toList());
+			Integer current = Integer.valueOf(0);
+			List<Integer> freqs = new ArrayList<>();
+			Integer result = null;
+			while (result == null) {
+				for (Integer i : ints) {
+					current = current + i;
+					if (freqs.contains(current)) {
+						result = current;
+						break;
+					} else {
+						freqs.add(current);
+					}
+				}
+			}
+			System.out.println("Day1.main: result = " + result);
+		}
 	}
 }
